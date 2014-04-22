276f0269d9481a75069f1de60a34d0262f121a76
Spartanace1024 <spartanace1024@gmail.com>
2014-04-22 03:13:01 -0700

    added physics materials and bouncy ball to scene
    removed raycast hit triggers from project settings

---
+++ Assets/Scripts/BallForce.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallForce : MonoBehaviour {
+    public bool useForce = false;
+    public float forceAmount = 2;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (useForce) {
+            rigidbody.AddForce(new Vector3(0, 0, -forceAmount));
+            useForce = false;
+        }
+	}
+}
